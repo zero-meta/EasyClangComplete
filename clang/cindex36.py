83579ea6d8611e60c7c05c6626f2e9327d3bea0e
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2016-04-24 11:01:58 +0200

    fix to the liblcang binary for libclang 3.6

--- clang/cindex36.py
+++ clang/cindex36.py
@@ -3519,7 +3519,7 @@ class Config:
         elif name == 'Windows':
             file = 'libclang.dll'
         else:
-            file = 'libclang.so'
+            file = 'libclang-3.6.so.1'
 
         if Config.library_path:
             file = Config.library_path + '/' + file

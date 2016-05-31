b8377e4de0a26926a17370942a8d008c0465427c
Ventero <ventero@ventero.de>
2016-05-31 18:53:55 +0200

    Refactor tests to reduce code duplication.

--- tests/test_complete.py
+++ tests/test_complete.py
@@ -9,7 +9,7 @@ from unittest import TestCase
 
 sys.path.append(path.dirname(path.dirname(__file__)))
 from plugin.plugin_settings import Settings
-from plugin.completion.bin_complete import Completer
+from plugin.completion.bin_complete import Completer as CompleterBin
 from plugin.completion.lib_complete import Completer as CompleterLib
 
 def has_libclang():
@@ -20,35 +20,76 @@ def has_libclang():
             return False
         return False
 
-class test_complete_command(TestCase):
-    """Test complete commands
+class base_test_complete(object):
+    """
+    Base class for all tests that are independent of the Completer implementation.
 
     Attributes:
         view (sublime.View): view
+        Completer (type): Completer class to use
     """
     def setUp(self):
-        """Set up the file to autocomplete
+        """ Setup method run before every test. """
 
-        """
-        file_name = path.join(path.dirname(__file__), 'test.cpp')
-        self.view = sublime.active_window().open_file(file_name)
-        while self.view.is_loading():
-            time.sleep(0.1)
-        # make sure we have a window to work with
+        # Ensure we have a window to work with.
         s = sublime.load_settings("Preferences.sublime-settings")
         s.set("close_windows_when_empty", False)
 
-    def tearDown(self):
-        """close the file. Finish test.
+        self.view = None
 
-        """
+    def tearDown(self):
+        """ Cleanup method run after every test. """
+
+        # If we have a view, close it.
         if self.view:
             self.view.set_scratch(True)
             self.view.window().focus_view(self.view)
             self.view.window().run_command("close_file")
+            self.view = None
+
+    def setUpView(self, filename):
+        """
+        Utility method to set up a view for a given file.
+
+        Args:
+            filename (str): The filename to open in a new view.
+        """
+
+        # Open the view.
+        file_path = path.join(path.dirname(__file__), filename)
+        self.view = sublime.active_window().open_file(file_path)
+
+        # Ensure it's loaded.
+        while self.view.is_loading():
+            time.sleep(0.1)
+
+    def setUpCompleter(self):
+        """
+        Utility method to set up a completer for the current view.
+
+        Returns:
+            BaseCompleter: completer for the current view.
+        """
+
+        settings = Settings()
+        current_folder = path.dirname(self.view.file_name())
+        parent_folder = path.dirname(current_folder)
+        include_dirs = settings.populate_include_dirs(
+            file_current_folder=current_folder,
+            file_parent_folder=parent_folder)
+
+        clang_binary = settings.clang_binary
+        completer = self.Completer(clang_binary)
+        completer.init(
+            view=self.view,
+            includes=include_dirs,
+            settings=settings)
+
+        return completer
 
     def getRow(self, row):
-        """Get text of a particular row
+        """
+        Get text of a particular row
 
         Args:
             row (int): number of row
@@ -58,10 +99,10 @@ class test_complete_command(TestCase):
         """
         return self.view.substr(self.view.line(self.view.text_point(row, 0)))
 
-    def test_setup(self):
-        """Test that the initial setup is valid
+    def test_setup_view(self):
+        """ Test that setup view correctly sets up the view. """
+        self.setUpView('test.cpp')
 
-        """
         file_name = path.join(path.dirname(__file__), 'test.cpp')
         self.assertEqual(self.view.file_name(), file_name)
         file = open(file_name, 'r')
@@ -74,200 +115,79 @@ class test_complete_command(TestCase):
         file.close()
 
     def test_init(self):
-        """Test that completer version is properly initialized
+        """ Test that the completer is properly initialized. """
+        self.setUpView('test.cpp')
+        completer = self.setUpCompleter()
 
-        """
-        completer = Completer("clang++")
-        self.assertIsNotNone(completer.version_str)
-        print("version is: {}".format(completer.version_str))
-
-    def test_init_lib(self):
-        """Test that completer version is properly initialized
-
-        """
-        if not has_libclang():
-            # we don't check libclang on platforms that lack its support
-            return
-        completer = CompleterLib("clang++")
-        self.assertIsNotNone(completer.version_str)
-        print("version is: {}".format(completer.version_str))
-
-    def test_init_completer(self):
-        """Test that completer is properly initialized
-
-        """
-        settings = Settings()
-        current_folder = path.dirname(self.view.file_name())
-        parent_folder = path.dirname(current_folder)
-        include_dirs = settings.populate_include_dirs(
-            file_current_folder=current_folder,
-            file_parent_folder=parent_folder)
-        completer = Completer("clang++")
-        completer.init(view=self.view,
-                       includes=include_dirs,
-                       settings=settings)
         self.assertTrue(completer.exists_for_view(self.view.id()))
-
-    def test_init_completer_lib(self):
-        """Test that completer is properly initialized
-
-        """
-        if not has_libclang():
-            return
-        settings = Settings()
-        current_folder = path.dirname(self.view.file_name())
-        parent_folder = path.dirname(current_folder)
-        include_dirs = settings.populate_include_dirs(
-            file_current_folder=current_folder,
-            file_parent_folder=parent_folder)
-        completer = CompleterLib("clang++")
-        completer.init(view=self.view,
-                       includes=include_dirs,
-                       settings=settings)
-        self.assertTrue(completer.exists_for_view(self.view.id()))
+        self.assertIsNotNone(completer.version_str)
 
     def test_complete(self):
-        """Test autocompletion for user type
+        """ Test autocompletion for user type. """
+        self.setUpView('test.cpp')
 
-        """
-        file_name = path.join(path.dirname(__file__), 'test.cpp')
-        self.view = sublime.active_window().open_file(file_name)
-        while self.view.is_loading():
-            time.sleep(0.1)
-        # now the file should be ready
-        settings = Settings()
-        current_folder = path.dirname(self.view.file_name())
-        parent_folder = path.dirname(current_folder)
-        include_dirs = settings.populate_include_dirs(
-            file_current_folder=current_folder,
-            file_parent_folder=parent_folder)
-        completer = Completer("clang++")
-        completer.init(view=self.view,
-                       includes=include_dirs,
-                       settings=settings)
+        completer = self.setUpCompleter()
         self.assertTrue(completer.exists_for_view(self.view.id()))
+
+        # Check the current cursor position is completable.
         self.assertEqual(self.getRow(5), "  a.")
         pos = self.view.text_point(5, 4)
         current_word = self.view.substr(self.view.word(pos))
         self.assertEqual(current_word, ".\n")
+
+        # Load the completions.
+        settings = Settings()
         completer.complete(self.view, pos, settings.errors_on_save)
+
+        # Wait 2 seconds for them to load.
         counter = 0
         while not completer.async_completions_ready:
             time.sleep(0.1)
             counter += 1
             if counter > 20:
-                break
+                self.fail("Async completions not ready after %d tries" % counter)
+
+        # Verify that we got the expected completions back.
         self.assertIsNotNone(completer.completions)
         expected = ['a\tint a', 'a']
-        self.assertTrue(expected in completer.completions)
-
-    def test_complete_lib(self):
-        """Test autocompletion for user type
-
-        """
-        if not has_libclang():
-            return
-        file_name = path.join(path.dirname(__file__), 'test.cpp')
-        self.view = sublime.active_window().open_file(file_name)
-        while self.view.is_loading():
-            time.sleep(0.1)
-        # now the file should be ready
-        settings = Settings()
-        current_folder = path.dirname(self.view.file_name())
-        parent_folder = path.dirname(current_folder)
-        include_dirs = settings.populate_include_dirs(
-            file_current_folder=current_folder,
-            file_parent_folder=parent_folder)
-        completer = CompleterLib("clang++")
-        completer.init(view=self.view,
-                       includes=include_dirs,
-                       settings=settings)
-        self.assertTrue(completer.exists_for_view(self.view.id()))
-        self.assertEqual(self.getRow(5), "  a.")
-        pos = self.view.text_point(5, 4)
-        current_word = self.view.substr(self.view.word(pos))
-        self.assertEqual(current_word, ".\n")
-        completer.complete(self.view, pos, settings.errors_on_save)
-        counter = 0
-        while not completer.async_completions_ready:
-            time.sleep(0.1)
-            counter += 1
-            if counter > 20:
-                break
-        self.assertIsNotNone(completer.completions)
-        expected = ['a\tint a', 'a']
-        self.assertTrue(expected in completer.completions)
+        self.assertIn(expected, completer.completions)
 
     def test_complete_vector(self):
-        """Test completion for std::vector
+        """ Test that we can complete vector members. """
+        self.setUpView('test_vector.cpp')
 
-        """
-        file_name = path.join(path.dirname(__file__), 'test_vector.cpp')
-        self.view = sublime.active_window().open_file(file_name)
-        while self.view.is_loading():
-            time.sleep(0.1)
-        # now the file should be ready
-        settings = Settings()
-        current_folder = path.dirname(self.view.file_name())
-        parent_folder = path.dirname(current_folder)
-        include_dirs = settings.populate_include_dirs(
-            file_current_folder=current_folder,
-            file_parent_folder=parent_folder)
-        completer = Completer("clang++")
-        completer.init(view=self.view,
-                       includes=include_dirs,
-                       settings=settings)
+        completer = self.setUpCompleter()
         self.assertTrue(completer.exists_for_view(self.view.id()))
+
+        # Check the current cursor position is completable.
         self.assertEqual(self.getRow(3), "  vec.")
         pos = self.view.text_point(3, 6)
         current_word = self.view.substr(self.view.word(pos))
         self.assertEqual(current_word, ".\n")
+
+        # Load the completions.
+        settings = Settings()
         completer.complete(self.view, pos, settings.errors_on_save)
+
+        # Wait 2 seconds for them to load.
         counter = 0
         while not completer.async_completions_ready:
             time.sleep(0.1)
             counter += 1
             if counter > 20:
-                break
-        print(completer.completions[:10])
+                self.fail("Async completions not ready after %d tries" % counter)
+
+        # Verify that we got the expected completions back.
         self.assertIsNotNone(completer.completions)
         expected = ['begin\titerator begin()', 'begin()']
-        self.assertTrue(expected in completer.completions)
+        self.assertIn(expected, completer.completions)
 
-    def test_complete_vector_lib(self):
-        """Test completion for std::vector
+# Define the actual test class implementations.
+class test_bin_complete(base_test_complete, TestCase):
+    """ Test class for the binary based completer. """
+    Completer = CompleterBin
 
-        """
-        if not has_libclang():
-            return
-        file_name = path.join(path.dirname(__file__), 'test_vector.cpp')
-        self.view = sublime.active_window().open_file(file_name)
-        while self.view.is_loading():
-            time.sleep(0.1)
-        # now the file should be ready
-        settings = Settings()
-        current_folder = path.dirname(self.view.file_name())
-        parent_folder = path.dirname(current_folder)
-        include_dirs = settings.populate_include_dirs(
-            file_current_folder=current_folder,
-            file_parent_folder=parent_folder)
-        completer = CompleterLib("clang++")
-        completer.init(view=self.view,
-                       includes=include_dirs,
-                       settings=settings)
-        self.assertTrue(completer.exists_for_view(self.view.id()))
-        self.assertEqual(self.getRow(3), "  vec.")
-        pos = self.view.text_point(3, 6)
-        current_word = self.view.substr(self.view.word(pos))
-        self.assertEqual(current_word, ".\n")
-        completer.complete(self.view, pos, settings.errors_on_save)
-        counter = 0
-        while not completer.async_completions_ready:
-            time.sleep(0.1)
-            counter += 1
-            if counter > 20:
-                break
-        print(completer.completions[:10])
-        self.assertIsNotNone(completer.completions)
-        expected = ['begin\titerator begin()', 'begin()']
-        self.assertTrue(expected in completer.completions)
+if has_libclang():
+    class test_lib_complete(base_test_complete, TestCase):
+        """ Test class for the library based completer. """
+        Completer = CompleterLib

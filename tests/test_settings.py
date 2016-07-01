81aa4cbd87b592f8132462642d649b11f445d56c
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2016-07-01 22:38:27 +0200

    fix settings tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,6 +1,8 @@
 """Tests for settings
 """
+import sublime
 import sys
+import time
 from os import path
 from unittest import TestCase
 
@@ -11,6 +13,39 @@ from plugin.plugin_settings import Settings
 class test_settings(TestCase):
     """Tests for settings
     """
+    def setUp(self):
+        """Set up testing environment
+        """
+        self.view = None
+        # make sure we have a window to work with
+        s = sublime.load_settings("Preferences.sublime-settings")
+        s.set("close_windows_when_empty", False)
+
+    def setUpView(self, filename):
+        """
+        Utility method to set up a view for a given file.
+
+        Args:
+            filename (str): The filename to open in a new view.
+        """
+        # Open the view.
+        file_path = path.join(path.dirname(__file__), filename)
+        self.view = sublime.active_window().open_file(file_path)
+
+        # Ensure it's loaded.
+        while self.view.is_loading():
+            time.sleep(0.1)
+
+    def tearDown(self):
+        """ Cleanup method run after every test. """
+
+        # If we have a view, close it.
+        if self.view:
+            self.view.set_scratch(True)
+            self.view.window().focus_view(self.view)
+            self.view.window().run_command("close_file")
+            self.view = None
+
     def test_init(self):
         """Test that settings are correctly initialized
 
@@ -38,6 +73,10 @@ class test_settings(TestCase):
     def test_populate_includes(self):
         """Testing include population
         """
+        # open any existing file
+        self.tearDown()
+        self.setUpView('test_wrong_triggers.cpp')
+        # now test the things
         settings = Settings()
         self.assertTrue(settings.is_valid())
         settings.include_file_folder = True
@@ -47,10 +86,12 @@ class test_settings(TestCase):
             path.realpath("/test/test")
         ]
         initial_dirs = list(settings.include_dirs)
-        dirs = settings.populate_include_dirs(path.realpath(__file__),
-                                              path.abspath(path.curdir))
+        dirs = settings.populate_include_dirs(self.view)
+
+        current_folder = path.dirname(self.view.file_name());
+        parent_folder = path.dirname(current_folder);
         self.assertLess(len(initial_dirs), len(dirs))
         self.assertNotEqual(dirs[0], initial_dirs[0])
         self.assertEqual(dirs[1], initial_dirs[1])
-        self.assertEqual(dirs[2], path.realpath(__file__))
-        self.assertEqual(dirs[3], path.abspath(path.dirname(path.curdir)))
+        self.assertEqual(dirs[2], current_folder)
+        self.assertEqual(dirs[3], parent_folder)

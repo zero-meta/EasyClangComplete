fe4e883cd7cc51a89dad8e71c83ccc3b94d02f35
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2016-08-29 12:28:21 +0200

    should fix the build error

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -181,13 +181,10 @@ class test_file(TestCase):
         """
         current_folder = path.dirname(path.abspath(__file__))
         parent_folder = path.dirname(current_folder)
-        print('current folder', current_folder)
-        print('parent folder', parent_folder)
         file = File.search(
-             file_name='.clang_complete',
+             file_name='.travis.yml',
              from_folder=current_folder,
              to_folder=parent_folder)
-        expected = path.join(parent_folder, '.clang_complete')
-        print("is", file.full_path(),"equal to", expected,"?")
+        expected = path.join(parent_folder, '.travis.yml')
         self.assertTrue(file.loaded())
         self.assertEqual(file.full_path(), expected)

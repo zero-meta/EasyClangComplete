7269d263e0ae3ea233662482132e30adfc148a2e
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2016-11-20 23:26:26 +0100

    fix module annotation

--- plugin/flags_sources/__init__.py
+++ plugin/flags_sources/__init__.py
@@ -1,1 +1,2 @@
-__all__ = ["flags_source", "clang_complete", "cmake_flags", "compilation_db"]
+"""A module for generation of flags from a specified flag source."""
+__all__ = ["flags_source", "flags_file", "cmake_file", "compilation_db"]

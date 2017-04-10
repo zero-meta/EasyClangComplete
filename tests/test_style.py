29bab06170a3ac93756022990ca0f2246dd03757
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2017-04-10 22:25:09 +0200

    fix windows build too

--- tests/test_style.py
+++ tests/test_style.py
@@ -9,6 +9,9 @@ PEP8_CMD = "pep8 {} --exclude=clang --count --max-line-length=80"
 
 PLUGIN_SOURCE_FOLDER = path.dirname(path.dirname(__file__))
 
+LINUX_MISSING_MSG = "command not found"
+WINDOWS_MISSING_MSG = "is not recognized as an internal or external command"
+
 
 class TestStyle(TestCase):
     """Test that the code conforms to pep8 and pep257."""
@@ -18,7 +21,7 @@ class TestStyle(TestCase):
         cmd = PEP8_CMD.format(PLUGIN_SOURCE_FOLDER)
         output = Tools.run_command(cmd)
         print(output)
-        if 'command not found' in output:
+        if LINUX_MISSING_MSG in output or WINDOWS_MISSING_MSG in output:
             print('no pep8 found in path!')
             return
         self.assertTrue(len(output) == 0)
@@ -28,7 +31,7 @@ class TestStyle(TestCase):
         cmd = PEP257_CMD.format(PLUGIN_SOURCE_FOLDER)
         output = Tools.run_command(cmd)
         print(output)
-        if 'command not found' in output:
+        if LINUX_MISSING_MSG in output or WINDOWS_MISSING_MSG in output:
             print('no pep257 found in path!')
             return
         self.assertTrue(len(output) == 0)

a1b27fc49d46525a7752b557c75de85096ec2f5b
Maximilian Heinzler <m.heinzler@heinzler.de>
2018-04-17 22:24:01 +0200

    Fix handling of file bodies with multi-byte characters (#450)
    Always calculate the length of file contents from their bytes
    representation to not cut off characters at the end when the string
    contains multi-byte characters.
    
    Fix #443

--- plugin/clang/cindex34.py
+++ plugin/clang/cindex34.py
@@ -2181,7 +2181,7 @@ class TranslationUnit(ClangObject):
 
                 unsaved_array[i].name = name.encode('utf8')
                 unsaved_array[i].contents = contents.encode('utf8')
-                unsaved_array[i].length = len(contents)
+                unsaved_array[i].length = len(unsaved_array[i].contents)
 
         ptr = conf.lib.clang_parseTranslationUnit(index, filename.encode('utf8'), args_array,
                                     len(args), unsaved_array,
@@ -2363,7 +2363,8 @@ class TranslationUnit(ClangObject):
                     raise TypeError('Unexpected unsaved file contents.')
                 unsaved_files_array[i].name = name.encode('utf-8')
                 unsaved_files_array[i].contents = value.encode('utf-8')
-                unsaved_files_array[i].length = len(value)
+                unsaved_files_array[i].length = \
+                    len(unsaved_files_array[i].contents)
         ptr = conf.lib.clang_reparseTranslationUnit(self, len(unsaved_files),
                 unsaved_files_array, options)
 
@@ -2427,7 +2428,8 @@ class TranslationUnit(ClangObject):
                     raise TypeError('Unexpected unsaved file contents.')
                 unsaved_files_array[i].name = name.encode('utf8')
                 unsaved_files_array[i].contents = value.encode('utf8')
-                unsaved_files_array[i].length = len(value)
+                unsaved_files_array[i].length = \
+                    len(unsaved_files_array[i].contents)
         ptr = conf.lib.clang_codeCompleteAt(self, path.encode('utf8'), line, column,
                 unsaved_files_array, len(unsaved_files), options)
         if ptr:

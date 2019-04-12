fa8f74ceb5444f5c8d4164159ed668b1c7b31570
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2019-04-12 20:53:52 +0200

    Only hide default completions if we have ours

--- plugin/tools.py
+++ plugin/tools.py
@@ -155,7 +155,7 @@ class SublBridge:
         Returns:
             tuple: (completions, flags)
         """
-        if hide_default_completions:
+        if completions and hide_default_completions:
             log.debug("hiding default completions")
             return (completions, SublBridge.NO_DEFAULT_COMPLETIONS)
         else:

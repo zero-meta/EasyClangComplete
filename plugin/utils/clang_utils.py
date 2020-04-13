18907c612e28ba2f505ab0a4a21591c675c35688
Jan Ekström <jeebjp@gmail.com>
2020-04-13 14:31:49 +0300

    Add clang 9/10 SONAMEs for Linux (#687)

--- plugin/utils/clang_utils.py
+++ plugin/utils/clang_utils.py
@@ -27,7 +27,7 @@ class ClangUtils:
         WINDOWS_SUFFIXES (list): suffixes for windows
     """
     WINDOWS_SUFFIXES = ['.dll', '.lib']
-    LINUX_SUFFIXES = ['.so', '.so.1', '.so.7', '.so.8']
+    LINUX_SUFFIXES = ['.so', '.so.10', '.so.9', '.so.8', '.so.7', '.so.1']
     OSX_SUFFIXES = ['.dylib']
 
     SUFFIXES = {
